ff7492848bc716b6b83f5059214d6ae80845154c
Serina Robinson <robi0916@umn.edu>
2019-01-19 14:29:36 -0600

    Updated sample data

--- article_scratch.r
+++ article_scratch.r
@@ -1,51 +1,52 @@
-### Return most frequent string for each group
 
 # Install packages
 
-library(tidyverse)
-# Create sample data
-# @Rscotty provided dataset
-sample <- tibble::tribble(
-  ~Patient_ID, ~Status,
-  1L,                  "G",
-  1L,                  "G",
-  1L,                  "B",
-  2L,                  "G",
-  2L,                  "B",
-  3L,                  "O",
-  3L,                  "B",
-  3L,                  "G",
-  4L,                  "X",
-  4L,                  "B"
-)
+# library(tidyverse)
+# install.packages("datapasta")
+# library(datapasta)
 
-# Workflow to return most frequent string
+# Create a sample data for different patient groups
+my_df <- data.frame(patient_grp = factor(rep(LETTERS[1:6], each=3)),
+                    a = rep(1:2),
+                    b = c(11:13, 21:23, 31:33, 41:43, 51:53, 61:63))
 
-sample %>%
+# Create a tibble
+my_tibb <- my_df %>% 
+  as_tibble()
+
+### Return most frequent string for each group
+
+my_tibb %>%
   # add a column n with count by categories
-  add_count(Patient_ID, Status) %>%
+  add_count(patient_grp, a) %>%
   # select max or first occurrence by patient
-  group_by(Patient_ID) %>%
+  group_by(patient_grp) %>%
   # keep only first TRUE
-  mutate(Majority = Status[n == max(n)][1]) %>%
+  mutate(majority = a[n == max(n)][1]) %>%
   # do not keep temp var
   select(-n)
 
-### Sort within each group
+
+### Sort differently based on group membership
+
+## Overall workflow
 
 # 1. Create a sorted column
 # 2. Use case_when to selectively use the sorted or the original values.
 
-# Create sample data
-library(dplyr)
+# The desired vector looks like this
+b = c(11:13, 21:23, 33:31, 41:43, 51:53, 61:63)
 
-my.df <- data.frame(aa=factor(c(1:24)), 
-                    bb=factor(rep(c(1:6), each=4)),
-                    cc=factor(rep(c(1,2), each=4, times=3)),
-                    dd=c(11:14, 21:24, 31:34, 41:44, 51:54, 61:64),
-                    desired=c(11:14, 24:21, 31:34, 44:41, 51:54, 64:61))
-my.df %>%
-  group_by(bb) %>%
-  mutate(dd_sorted = sort(dd, decreasing = TRUE)) %>%
-  mutate(dd_new = case_when(cc == 2 ~ dd_sorted,
-                            TRUE ~ dd))
+# i.e. when the patient group is C, the order of column b is reversed
+my_tibb
+
+my_tibb %>%
+  group_by(patient_grp) %>%
+  mutate(b_sorted = sort(b, decreasing = TRUE)) %>%
+  mutate(b_desired = case_when(patient_grp == "C" ~ b_sorted,
+                              TRUE ~ b))
+
+# You can then remove any columns you don't want to keep
+  
+
+
